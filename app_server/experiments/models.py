fbf9f7f92bb92e6ffc0292d5ef96d0ac366bed8b
Lucile <lucile.billard@yahoo.fr>
2018-02-22 23:55:08 +0100

    models updated

--- app_server/experiments/models.py
+++ app_server/experiments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Instance(models.Model):
+    name = models.CharField(max_length=100)
+    ptype = models.CharField(max_length=30)
+    path = models.CharField(max_length=200)
+
+class Instance_feature(models.Model):
+    name = models.CharField(max_length=100)
+    unit = models.CharField(max_length=30)
