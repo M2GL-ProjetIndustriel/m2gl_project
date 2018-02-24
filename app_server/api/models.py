796a9c04a4077b66a7702f616bf55497dbb3cc9f
Liuko <whydah246@gmail.com>
2018-02-24 17:36:16 +0100

    Result + experimentation models

--- app_server/api/models.py
+++ app_server/api/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Instance_feature(models.Model):
     name = models.CharField(max_length=100)
     value = models.CharField(max_length=100)
-    unit = models.CharField(max_length=100)
+    unit = models.CharField(max_length=100, blank=True)
 
 class Instance(models.Model):
     name = models.CharField(max_length=100) # name of the problem ex: "100-queens"
@@ -18,3 +18,19 @@ class Solver(models.Model):
     version = models.CharField(max_length=100, blank=True)
     source_path = models.CharField(max_length=200, blank=True) # temporary until we find a better way of handling files.
     executable_path = models.CharField(max_length=200, blank=True) # temporary until we find a better way of handling files.
+
+class Experimentation(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateField()
+    solver_parameters = models.CharField(max_length=200)
+    device_info = models.CharField(max_length=200, blank=True)
+
+class Result(models.Model):
+    status = models.CharField(max_length=100)
+    result = models.ForeignKey(Experimentation, on_delete=models.CASCADE)
+
+class Result_Measurement(models.Model):
+    name = models.CharField(max_length=100)
+    value = models.DecimalField(max_digits=20, decimal_places=10)
+    unit = models.CharField(max_length=100, blank=True)
+    result = models.ForeignKey(Result, on_delete=models.CASCADE)
