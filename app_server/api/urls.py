9c88213ef8670673c343cc5fda4484c952942988
Lucile <lucile.billard@yahoo.fr>
2018-02-24 16:32:55 +0100

    url for solvers added

--- app_server/api/urls.py
+++ app_server/api/urls.py
@@ -5,6 +5,6 @@ from api import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('instances/', views.InstanceList.as_view()),
-    path(r'^instances/(?P<pk>[0-9]+)$', views.InstanceDetail.as_view())
-    path('solvers/', views.SolverList.as_view()),
+    path(r'^instances/(?P<pk>[0-9]+)$', views.InstanceDetail.as_view()),
+    path('solvers/', views.SolverList.as_view())
 ]
