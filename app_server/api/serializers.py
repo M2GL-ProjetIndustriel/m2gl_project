255aca1a76d9ce7ab66e770631d5d5edd1776e5c
Lucile <lucile.billard@yahoo.fr>
2018-02-24 16:23:59 +0100

    serializers + views added

--- app_server/api/serializers.py
+++ app_server/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from api.models import Instance_feature, Instance
+from api.models import Instance_feature, Instance, Solver
 
 class InstanceSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,3 +15,18 @@ class InstanceSerializer(serializers.ModelSerializer):
         instance.path = validated_data.get('path', instance.path)
         instance.save()
         return instance
+
+class SolverSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Solver
+        fields = ('id', 'name', 'version', 'source_path', 'executable_path')
+
+    def create(self, validated_data):
+        return solver.objects.create(**validated_data)
+
+    def update(self, solver, validated_data):
+        solver.name = validated_data.get('name', solver.name)
+        solver.ptype = validated_data.get('ptype', solver.code)
+        solver.path = validated_data.get('path', solver.path)
+        solver.save()
+        return solver
