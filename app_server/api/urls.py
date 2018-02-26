0f44821e35c8d99ec41ce65a4eb8bcf177e27413
Lucile <lucile.billard@yahoo.fr>
2018-02-26 22:39:06 +0100

    views for solvers

--- app_server/api/urls.py
+++ app_server/api/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
-from api.views import InstanceList, InstanceDetail, SolverList
+from django.urls import path, re_path
+from api.views import InstanceList, InstanceDetail, SolverList, SolverDetail
 from api import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('instances/', views.InstanceList.as_view()),
-    path(r'^instances/(?P<pk>[0-9]+)$', views.InstanceDetail.as_view()),
-    path('solvers/', views.SolverList.as_view())
+    re_path(r'^instances/(?P<pk>[0-9]+)/$', views.InstanceDetail.as_view()),
+    path('solvers/', views.SolverList.as_view()),
+    re_path(r'^solvers/(?P<pk>[0-9]+)/$', views.SolverDetail.as_view())
 ]
--- app_server/api/views.py
+++ app_server/api/views.py
@@ -8,7 +8,6 @@ from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework import status
 
-
 def index(request):
     return HttpResponse("Hello, world. You're at the api index.")
 
@@ -73,3 +72,27 @@ class SolverList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@permission_classes((permissions.AllowAny,))
+class SolverDetail(APIView):
+    """
+    Retrieve, update or delete a solver.
+    """
+
+    def get(self, request, pk, format=None):
+        solver = get_object_or_404(Solver, pk=pk)
+        serializer = SolverSerializer(solver)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        solver = get_object_or_404(Solver, pk=pk)
+        serializer = SolverSerializer(solver, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        solver = get_object_or_404(Solver, pk=pk)
+        solver.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
