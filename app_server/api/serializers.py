8050b485c34780d56c893d0b9f5e3ce881a9b6ab
Lucile <lucile.billard@yahoo.fr>
2018-02-27 16:42:09 +0100

    serializers shrinked

--- app_server/api/serializers.py
+++ app_server/api/serializers.py
@@ -6,51 +6,13 @@ class InstanceSerializer(serializers.ModelSerializer):
         model = Instance
         fields = ('id', 'name', 'ptype', 'path')
 
-    def create(self, validated_data):
-        return Instance.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.ptype = validated_data.get('ptype', instance.ptype)
-        instance.path = validated_data.get('path', instance.path)
-        instance.save()
-        return instance
-
 class SolverSerializer(serializers.ModelSerializer):
     class Meta:
         model = Solver
         fields = ('id', 'name', 'version', 'source_path', 'executable_path')
-
-    def create(self, validated_data):
-        return Solver.objects.create(**validated_data)
-
-    def update(self, solver, validated_data):
-        solver.name = validated_data.get('name', solver.name)
-        solver.source_path = validated_data.get('source_path', solver.source_path)
-        solver.executable_path = validated_data.get('executable_path', solver.executable_path)
-        solver.save()
-        return solver
-
 
 class ExperimentationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Experimentation
         fields = ('id', 'name', 'date', 'solver_parameters', 'solver',
             'device_info')
-
-    # vvv Inutile?
-    """
-    def create(self, validated_data):
-        return experimentation.objects.create(**validated_data)
-
-    def update(self, solver, validated_data):
-        experimentation.name = validated_data.get('name', experimentation.name)
-        experimentation.date = validated_data.get('date', experimentation.date)
-        experimentation.solver_parameters = validated_data.get('solver_parameters',
-            experimentation.solver_parameters)
-        experimentation.solver = validated_data.get('solver', experimentation.solver)
-        experimentation.device_info = validated_data.get('device_info',
-            experimentation.device_info)
-        experimentation.save()
-        return experimentation
-    """
