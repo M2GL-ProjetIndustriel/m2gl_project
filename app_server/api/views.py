fac6c00e2e7363918c18861899c4d67d537083fa
Liuko <whydah246@gmail.com>
2018-02-27 15:58:20 +0100

    Refactore view

--- app_server/api/views.py
+++ app_server/api/views.py
@@ -14,88 +14,26 @@ def index(request):
 # API views
 
 @permission_classes((permissions.AllowAny,))
-class InstanceList(APIView):
-    """
-    List all instances, or create a new instance.
-    """
-
-    def get(self, request, format=None):
-        instances = Instance.objects.all()
-        serializer = InstanceSerializer(instances, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = InstanceSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class InstanceList(generics.ListCreateAPIView):
+    queryset = Instance.objects.all()
+    serializer_class = InstanceSerializer
 
 @permission_classes((permissions.AllowAny,))
-class InstanceDetail(APIView):
-    """
-    Retrieve, update or delete an instance (problem instance) .
-    """
+class InstanceDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Instance.objects.all()
+    serializer_class = InstanceSerializer
 
-    def get(self, request, pk, format=None):
-        instance = get_object_or_404(Instance, pk=pk)
-        serializer = InstanceSerializer(instance)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        instance = get_object_or_404(Instance, pk=pk)
-        serializer = InstanceSerializer(instance, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        instance = get_object_or_404(Instance, pk=pk)
-        instance.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @permission_classes((permissions.AllowAny,))
-class SolverList(APIView):
-    """
-    List all solvers, or create a new solver.
-    """
-
-    def get(self, request, format=None):
-        solvers = Solver.objects.all()
-        serializer = SolverSerializer(solvers, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = SolverSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class SolverList(generics.ListCreateAPIView):
+    queryset = Solver.objects.all()
+    serializer_class = SolverSerializer
 
 @permission_classes((permissions.AllowAny,))
-class SolverDetail(APIView):
-    """
-    Retrieve, update or delete a solver.
-    """
+class SolverDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Solver.objects.all()
+    serializer_class = SolverSerializer
 
-    def get(self, request, pk, format=None):
-        solver = get_object_or_404(Solver, pk=pk)
-        serializer = SolverSerializer(solver)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        solver = get_object_or_404(Solver, pk=pk)
-        serializer = SolverSerializer(solver, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        solver = get_object_or_404(Solver, pk=pk)
-        solver.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 @permission_classes((permissions.AllowAny,))
 class ExperimentationList(generics.ListCreateAPIView):
