902c4ebbfe97685e2e690db8549a36fac9baae23
Lucile <lucile.billard@yahoo.fr>
2018-02-27 01:51:02 +0100

    pytest

--- app_server/api/tests.py
+++ app_server/api/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory
 
-# Create your tests here.
+#factory = APIRequestFactory()
+#request = factory.post('/api/instances/', {'name': 'nqueens', 'ptype':'CSP', 'path':'path1'})
+
+
+def test_101():
+    assert 10 % 2 == 0
