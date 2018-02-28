c5da281b0ef7444d380d4cc3dacbdebde1ec11e4
Fodil <21066051+AFodil@users.noreply.github.com>
2018-02-28 21:51:08 +0100

    created a test, fixed time_zone, added more test patterns (recommended ones), added requirement (not needed but will be useful later).

--- app_server/api/tests.py
+++ app_server/api/tests.py
@@ -1,17 +1,20 @@
-from django.test import TestCase
-from rest_framework.test import APIRequestFactory
-from .models import Solver, Instance
 import pytest
+from rest_framework.test import APITestCase
 
-def test_101():
-    assert 10 % 2 == 0
+from api.models import Solver
 
-@pytest.mark.django_db
-def test_solver_post():
-    factory = APIRequestFactory()
-    data = {'name': 'choco', 'version':'2.0', 'source_path':'path1', 'executable_path': 'path2'}
-    request = factory.post('/solvers/', data)
-    queryset = Solver.objects.all()
-    assert len(queryset) > 0
-    solver = queryset[0]
-    assert solver.name == 'choco'
+
+class SolverTest(APITestCase):
+    @pytest.mark.django_db()
+    def test_correct_solver_post_should_return_201(self):
+        response = self.client.post('/api/solvers/', {
+            'name': 'choco',
+            'version': '2.0',
+            'source_path': 'path1',
+            'executable_path': 'path2'
+        })
+        assert response.status_code == 201
+        queryset = Solver.objects.all()
+        assert len(queryset) > 0
+        solver = queryset[0]
+        assert solver.name == 'choco'
