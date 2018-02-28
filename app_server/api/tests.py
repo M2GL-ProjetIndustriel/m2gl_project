a60d1ea56d4220c63acf5dde03aaabd937a9fe5b
Lucile <lucile.billard@yahoo.fr>
2018-02-28 18:51:02 +0100

    failing test: Fix it.

--- app_server/api/tests.py
+++ app_server/api/tests.py
@@ -1,9 +1,17 @@
 from django.test import TestCase
 from rest_framework.test import APIRequestFactory
-
-#factory = APIRequestFactory()
-#request = factory.post('/api/instances/', {'name': 'nqueens', 'ptype':'CSP', 'path':'path1'})
-
+from .models import Solver, Instance
+import pytest
 
 def test_101():
     assert 10 % 2 == 0
+
+@pytest.mark.django_db
+def test_solver_post():
+    factory = APIRequestFactory()
+    data = {'name': 'choco', 'version':'2.0', 'source_path':'path1', 'executable_path': 'path2'}
+    request = factory.post('/solvers/', data)
+    queryset = Solver.objects.all()
+    assert len(queryset) > 0
+    solver = queryset[0]
+    assert solver.name == 'choco'
