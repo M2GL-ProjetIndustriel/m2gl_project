00f2c5ea0ee0b9a558e8ce0602eece48698e89aa
Liuko <whydah246@gmail.com>
2018-03-01 13:16:10 +0100

    Add Nicolas JSON format

--- app_server/app_server/settings.py
+++ app_server/app_server/settings.py
@@ -115,7 +115,12 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_RENDERER_CLASSES': (
+        'app_server.customRenderer.CustomJSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    )
+
 }
 
 # Internationalization
