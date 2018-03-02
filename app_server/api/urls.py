e51bad89172a1b7f396845e30a16b4b82660ce36
Liuko <whydah246@gmail.com>
2018-03-02 22:26:38 +0100

    Change url and bug fix for filename

--- app_server/api/urls.py
+++ app_server/api/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('instances/<int:pk>', views.InstanceDetail.as_view()),
     path('solver/', views.SolverList.as_view()),
     path('solver/<int:pk>', views.SolverDetail.as_view()),
-    path('solver/downloads/<str:filename>', views.SourcesFiles.as_view()),
+    path('solver/<int:pk>/source', views.DownloadFiles.as_view()),
+    path('solver/<int:pk>/executable', views.DownloadFiles.as_view()),
     path('experimentation/', views.ExperimentationList.as_view()),
     path('experimentation/<int:pk>', views.ExperimentationDetail.as_view())
 ]
--- app_server/api/views.py
+++ app_server/api/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404
 from rest_framework import permissions, generics
 from rest_framework.decorators import permission_classes
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.views import APIView
-import os
-
+import os.path
+import pathlib
+from urllib.parse import quote
 
 from .constants import *
 from .models import Instance, Solver, Experimentation
@@ -62,18 +64,31 @@ class ExperimentationDetail(generics.RetrieveUpdateDestroyAPIView):
 
 
 @permission_classes((permissions.AllowAny,))
-class SourcesFiles(APIView):
-    def get(self, request, filename, format=None):
-        file_name = request.path.split("/")[-1]
-        file_path = DOWNLOADS_PATH + file_name
+class DownloadFiles(APIView):
+    def get(self, request, pk, format=None):
+        solver = get_object_or_404(Solver.objects.all(), pk=pk)
+        #Choose between executable and source file
+        if request.path.split('/')[-1] == 'source':
+            file_path = solver.source_path.name
+            type_file = 'source'
+        else:
+            file_path = solver.executable_path.name
+            type_file = 'executable'
+        extension = ''.join(pathlib.Path(file_path).suffixes)
+        send_file_name = solver.name + '_' + type_file + extension
+        response = self.response_from_file(file_path, send_file_name)
+        return response
+
+    def response_from_file(self, file_path, send_file_name):
         try:
             fp = open(file_path, 'rb')
             response = HttpResponse(fp.read())
             fp.close()
+
             response['Content-Length'] = os.path.getsize(file_path)
+            filename_header = 'filename*=UTF-8\'\'%s' % quote(
+                send_file_name.encode('utf-8'))
+            response['Content-Disposition'] = 'attachment; ' + filename_header
         except FileNotFoundError:
             raise Http404("File does not exist")
-        else:
-            filename_header = 'filename=%s' % file_name
-            response['Content-Disposition'] = 'attachment; ' + filename_header
         return response
