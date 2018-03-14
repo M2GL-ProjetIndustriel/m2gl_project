b2d1a99819794e419e3859bce20818d2f3ee4f17
Liuko <whydah246@gmail.com>
2018-03-14 11:25:14 +0100

    Update settings

--- app_server/app_server/settings.py
+++ app_server/app_server/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
 
     'api.apps.ApiConfig',  # experiments app added.
+    'django_filters',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
